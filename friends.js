912aff56d094daf1a8bc8593b30a9455a333959d
WP1GYSC <rene.dakovic@audi.de>
2024-11-24 13:31:13 +0100

    V3

--- friends.js
+++ friends.js
@@ -1,1 +1,19 @@
 // https://online-lectures-cs.thi.de/chat/7c6a5231-9a5a-445c-bc84-6d2bc99600b1/user
+
+
+function listUsers() {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function () {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            let data = JSON.parse(xmlhttp.responseText);
+            console.log(data);
+        }
+    };
+
+    var requestUser = "https://online-lectures-cs.thi.de/chat/7c6a5231-9a5a-445c-bc84-6d2bc99600b1/user/" + userName;
+    xmlhttp.open("GET", requestUser, true);
+    xmlhttp.setRequestHeader('Authorization', 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiVG9tIiwiaWF0IjoxNzMyNDQ3MTI4fQ.7ZPiucbT3OJSxtWYKqxBCpkKsxPfjgHMr4LUS06Ef4Y');
+    xmlhttp.send();
+
+    return userExists;
+};
