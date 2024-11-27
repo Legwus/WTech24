0eaeb541c8872f091a7acf07e69840db41e2a546
Legwus <surichbuncher@gmail.com>
2024-11-27 02:20:53 +0100

    pls work

--- register.js
+++ register.js
@@ -1,69 +1,75 @@
 function checkIfUserExists(userName) {
-    var xmlhttp = new XMLHttpRequest();
-    var userExists = false;
-    xmlhttp.onreadystatechange = function() {
-        if(xmlhttp.readyState == 4) {
-            if(xmlhttp.status == 204) {
-                console.log("Username already exists.");
-                userExists = true;
-            } else if (xmlhttp.status == 404) {
-                console.log("Username available.");
-                userExists = false;
-            }
-        }
-    };
+  var xmlhttp = new XMLHttpRequest();
+  var userExists = false;
+  xmlhttp.onreadystatechange = function () {
+    if (xmlhttp.readyState == 4) {
+      if (xmlhttp.status == 204) {
+        console.log("Username already exists.");
+        userExists = true;
+      } else if (xmlhttp.status == 404) {
+        console.log("Username available.");
+        userExists = false;
+      }
+    }
+  };
 
-    var requestUser = "https://online-lectures-cs.thi.de/chat/7c6a5231-9a5a-445c-bc84-6d2bc99600b1/user/" + userName;
-    xmlhttp.open("GET", requestUser, false);
-    xmlhttp.send();
+  var requestUser =
+    "https://online-lectures-cs.thi.de/chat/7c6a5231-9a5a-445c-bc84-6d2bc99600b1/user/" +
+    userName;
+  xmlhttp.open("GET", requestUser, false);
+  xmlhttp.send();
 
-    return userExists;
-};
+  return userExists;
+}
 
 function checkUsernameInputLength() {
-    var inputField = document.getElementById("username");
-    if(inputField.value.length < 3) {
-        inputField.style.boxShadow = "inset 0 0 0 2px red";
-    } else {
-        inputField.style.boxShadow = "inset 0 0 0 2px green";
-    };
-};
+  var inputField = document.getElementById("username");
+  if (inputField.value.length < 3) {
+    inputField.style.borderColor = "red";
+  } else {
+    inputField.style.borderColor = "green";
+  }
+}
 
 function checkPasswordInputLength() {
-    var inputField = document.getElementById("password");
-    if(inputField.value.length < 8) {
-        inputField.style.boxShadow = "inset 0 0 0 2px red";
-        return false;
-    } else {
-        inputField.style.boxShadow = "inset 0 0 0 2px green";
-        return true;
-    };
-};
+  var passwordField = document.getElementById("password");
+  if (passwordField.value.length < 8) {
+    passwordField.style.borderColor = "red";
+  } else {
+    passwordField.style.borderColor = "green";
+  }
+}
 
 function checkPasswordConfirmInputLength() {
-    var inputField = document.getElementById("confirm_password");
-    if(inputField.value.length < 8) {
-        inputField.style.boxShadow = "inset 0 0 0 2px red";
-    } else {
-        inputField.style.boxShadow = "inset 0 0 0 2px green";
-    };
-};
+  var inputField = document.getElementById("confirm_password");
+  if (inputField.value.length < 8) {
+    inputField.style.borderColor = "red";
+  } else {
+    inputField.style.borderColor = "green";
+  }
+}
 
 function checkInputValidity() {
-    var usernameField = document.getElementById("username");
-    var passwordField = document.getElementById("password");
-    var confirmPasswordField = document.getElementById("confirm_password");
-    if(usernameField.value.length < 3) {
-        return false;
-    }
-    if((passwordField.value.length < 8) && (confirmPasswordField.value.length < 8)) {
-        return false;
-    }
-    if(passwordField.value != confirmPasswordField.value) {
-        return false;
-    }
-    if(checkIfUserExists(usernameField.value)) {
-        return false;
-    }
-    return true;
+  var usernameField = document.getElementById("username");
+  var passwordField = document.getElementById("password");
+  var confirmPasswordField = document.getElementById("confirm_password");
+  if (usernameField.value.length < 3) {
+    return false;
+  }
+  if (passwordField.value.length < 8 && confirmPasswordField.value.length < 8) {
+    passwordField.style.borderColor = "red";
+    return false;
+  }
+  if (passwordField.value != confirmPasswordField.value) {
+    confirmPasswordField.style.borderColor = "red";
+    passwordField.style.borderColor = "red";
+    return false;
+  }
+  if (checkIfUserExists(usernameField.value)) {
+    usernameField.style.borderColor = "red";
+    return false;
+  } else if (!checkIfUserExists(usernameField.value)) {
+    usernameField.style.borderColor = "green";
+  }
+  return true;
 }
